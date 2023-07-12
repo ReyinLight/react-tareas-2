5b627d066dd1f1ba6c5b8a5af4477b10c1ef64e4
Luis Reyes <ReyinLight@gmail.com>
2023-07-12 01:48:55 -0600

    Mi primer componente propio :D

---
+++ src/components/ComponentePro.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'preact/hooks'
+import React from 'react'
+
+export const ComponentePro = () => {
+
+  const [numero, setnumero] = useState(0)
+
+  const incrementar = () => {
+    setnumero((numero + 1));
+  }
+
+  return (
+    <>
+      <button onClick={incrementar}>picale aqui~ uwu</button><br />
+      <label> { numero } </label><br />
+      <button onClick={() => { setnumero(0) }}>muerele la suma &gt;:c</button><br />
+
+    </>
+  )
+}
